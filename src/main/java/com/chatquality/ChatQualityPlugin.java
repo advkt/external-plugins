adbd8960a9348ab0fa119f02fd9916f59b934d06
Jack Emerson <mail@jackemerson.me>
2023-02-13 13:34:33 +1000

    Initial chat quality setup

--- src/main/java/com/chatquality/ChatQualityPlugin.java
+++ src/main/java/com/chatquality/ChatQualityPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.chatquality;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class ChatQualityPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ChatQualityConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ChatQualityConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ChatQualityConfig.class);
 	}
 }
